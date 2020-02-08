77d4ccf073041a827f45f0624db6dbd7880971fc
Varya Stepanova <mail@varya.me>
2020-02-08 16:22:51 +0200

    Styling elements for layout

--- src/components/Layout/Layout--common.js
+++ src/components/Layout/Layout--common.js
@@ -2,12 +2,18 @@ import React from "react"
 import { graphql } from "gatsby"
 import { MDXRenderer } from "gatsby-plugin-mdx"
 
+import { Container, LeftSide, Content, RightSide } from "./Layout--elements";
+
 export default function PageTemplate({ data: { mdx } }) {
   return (
-    <div>
-      <h1>{mdx.frontmatter.title}</h1>
-      <MDXRenderer>{mdx.body}</MDXRenderer>
-    </div>
+    <Container>
+      <Content>
+        <h1>{mdx.frontmatter.title}</h1>
+        <MDXRenderer>{mdx.body}</MDXRenderer>
+      </Content>
+      <RightSide>1</RightSide>
+      <LeftSide>2</LeftSide>
+    </Container>
   )
 }
 
