a9137bc74fd4e90120fbcdffee0787d485a68324
Varya Stepanova <mail@varya.me>
2020-02-23 16:46:29 +0200

    Meta properties for Apple

--- src/html.js
+++ src/html.js
@@ -24,6 +24,9 @@ export default function HTML(props) {
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
         {props.headComponents}
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="#D0E0D8" />
+        <meta name="apple-mobile-web-app-title" content="Lazywill" />
         <link rel="apple-touch-icon" href="/icons/apple-icon-57x57.png" sizes="57x57" />
         <link rel="apple-touch-icon" href="/icons/apple-icon-60x60.png" sizes="60x60" />
         <link rel="apple-touch-icon" href="/icons/apple-icon-72x72.png" sizes="72x72" />
