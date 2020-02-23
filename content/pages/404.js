f793a4bdfff4a14df1469652e28e4d1fb70b5ef8
Varya Stepanova <mail@varya.me>
2020-02-23 17:00:27 +0200

    Providing location for 404 page

--- content/pages/404.js
+++ content/pages/404.js
@@ -3,8 +3,10 @@ import React from 'react'
 import Layout from '../../src/components/Layout/Layout--outer'
 import SEO from '../../src/components/seo'
 
-const NotFoundPage = () => (
-  <Layout>
+const NotFoundPage = ({
+  location,
+}) => (
+  <Layout location={location}>
     <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
